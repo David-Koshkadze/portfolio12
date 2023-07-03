1fcd755e853176945bba652a246be8d05b62a7ac
David-Koshkadze <rootUS3rr@pm.me>
2023-07-03 11:53:28 +0400

    home page intro

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import Image from "next/image";
+import photoMe from "../photome.jpg";
 
 export default function HomePage() {
   return (
-    <section id="home" className="h-screen">
-      Home
+    <section id="home" className="h-screen border border-yellow-300 pt-16">
+      <div className="flex">
+        <div>
+          <h1 className="text-5xl pb-10">David Koshkadze</h1>
+
+          <div className="text-white/50 w-1/2">
+            <p className="mb-3">
+              Welcome to my portfolio website, where innovation meets creativity
+              and code comes to life!
+            </p>
+            <p>
+              As a skilled web developer, I bring a passion for crafting
+              exceptional digital experiences that captivate users and drive
+              results.
+            </p>
+          </div>
+        </div>
+
+        {/* Right side */}
+        <div></div>
+      </div>
     </section>
   );
 }
